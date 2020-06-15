7221de49092ff362d33a2144552e862759dbae68
David Buzzell <xhamyd@gmail.com>
2020-06-14 21:30:14 -0400

    Test 6.1 complete

--- lesson_6_takehome.py
+++ lesson_6_takehome.py
@@ -2,16 +2,21 @@
 Lesson 6
 --------
 
-1)
+1) With a provided file pointer, check if a file has been properly closed or not (and then close the file if not!)
+
+    Return True for a closed file (is_closed(<file_closed 0x???>) == True).
+    Return False for an open file (is_closed(<file_open 0x???>) == False).
 
 Topics covered:
 
 """
 
+import os
+
 # --- DO NOT EDIT ABOVE THIS LINE --- #
 
 
-def is_palindrome(string_to_check):
+def is_closed(file_pointer):
     pass  # TODO: delete this entire line and complete this function
 
 
@@ -45,7 +50,7 @@ class TestCase:
 
     def run(self):
         res, outcome = True, []
-        for case_num in range(self.cases):
+        for case_num in range(1, self.cases+1):
             if (vals := self.test(self.func, case_num)) is not True:
                 res = False
                 in_val, out_val, actual_val = vals
@@ -56,16 +61,25 @@ class TestCase:
 
 # Test Cases
 def test_1(function_under_test, val_num):
-    in_vals = [1, 2, 3]
-    out_vals = [1, 4, 9]
+    assert val_num in (1, 2), ValueError(f"TESTING ERROR: Improper test case number provided: {val_num}")
+    if val_num == 1:
+        in_val, out_val = open("tempfile1.txt", "w"), True
+        in_val.close()
+        actual_val = function_under_test(in_val)
+        os.remove(in_val.name)
+        return True if actual_val == out_val else in_val, out_val, actual_val
+    else:
+        in_val, out_val = open("tempfile2.txt", "w"), False
+        actual_val = function_under_test(in_val)
+        if not in_val.closed:
+            in_val.close()
+            os.remove(in_val.name)
+            return in_val, out_val, actual_val
+        return True if actual_val == out_val else in_val, out_val, actual_val
 
-    in_val, out_val = in_vals[val_num], out_vals[val_num]
-    actual_val = function_under_test(in_val)
-    return True if out_val == actual_val else in_val, out_val, actual_val
 
-
-tcs = [TestCase(problem_number=1, description="Determine if a string is a palindrome.",
-                function_under_test=is_palindrome, testing_function=test_1, number_of_test_cases=3)]
+tcs = [TestCase(problem_number=1, description="Determine if a file is closed.", function_under_test=is_closed,
+                testing_function=test_1, number_of_test_cases=2)]
 
 for tc in tcs:
     print(f"Problem {tc.num}: {tc!s}")

8f2f0aed605e270137d0ce1ae316b270e8467ce9
David Buzzell <xhamyd@gmail.com>
2020-06-22 23:18:50 -0400

    Updates to Lesson 8

--- lesson_8_takehome.py
+++ lesson_8_takehome.py
@@ -4,17 +4,15 @@ Lesson 8
 
 1) Tetris Line Clear
 
-2) RegEx over text files
+2) Detect if a string contains a footnote marker
 
-3) Holiday Stats
+3) Find the mean, median, and range of 2020 holidays using the days within the year as your key metric. BONUS: Plot it!
 
-4) prep for NumPy/MatPlotLib
+4) prep for Music21
 
-5) prep for Music21
+5) Concatenate list of strings
 
-6) Optimization / Big O
-
-Topics covered: graphics, algorithms, file handling, text parsing
+Topics covered:
 
 """
 
@@ -47,7 +45,9 @@ class TestCase:
 
 
 # Test Cases
+# TODO: Check for docstrings in every test
 def test_1(function_under_test, val_num):
+    return True
     # TODO: Provide JPGs for each test case
     in_val, out_val = [['-'] * 8] * 10, False
     actual_val = function_under_test(in_val)
@@ -58,10 +58,25 @@ def test_2(function_under_test, val_num):
     return True
 
 
+def test_5(function_under_test, val_num):
+    in_val = random.choices(string.hexdigits.replace("abcdef", "-:-x"), k=10_000_000)
+    # ATOMIC_START
+    start_time = time.time()
+    out_val = function_under_test(in_val)
+    end_time = time.time()
+    # ATOMIC_END
+    assert "".join(in_val[:10]) == out_val[:10]  # TODO
+    time_threshold = 2.5
+    return True if end_time - start_time < time_threshold else ("[<random_characters>]", "execution_time < 5 sec",
+                                                                f"execution_time = {time_threshold} sec")
+
+
 tcs = [TestCase(problem_number=1, description="blank",
-                function_under_test=None, testing_function=test_1, number_of_test_cases=1),
+                function_under_test=tetris_line_clear, testing_function=test_1, number_of_test_cases=1),
        TestCase(problem_number=2, description="blank",
-                function_under_test=None, testing_function=test_2, number_of_test_cases=3)]
+                function_under_test=None, testing_function=test_2, number_of_test_cases=3),
+       TestCase(problem_number=5, description="blank",
+                function_under_test=combined_string, testing_function=test_5, number_of_test_cases=1)]
 
 for tc in tcs:
     print(f"Problem {tc.num}: {tc!s}")
